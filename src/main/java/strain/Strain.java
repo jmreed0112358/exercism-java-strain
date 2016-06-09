a5555456cd42b912d473978218328200b2d66f0b
Jeremy M. Reed <reeje76@gmail.com>
2016-06-09 08:57:44 -0600

    Implement keep / discard functions.

--- src/main/java/strain/Strain.java
+++ src/main/java/strain/Strain.java
@@ -6,21 +6,23 @@ import java.util.function.Predicate;
 
 public class Strain
 {
-
-	public Strain( )
-	{
-		// TODO Auto-generated constructor stub
-	}
-
 	public static <T> List< T > keep(List< T > input, Predicate<T> f) {
 		List<T> result = new LinkedList<T>();
-		
+		for ( T item : input ) {
+			if ( f.test( item )) {
+				result.add( item );
+			}
+		}
 		return result;
 	}
 	
 	public static <T> List< T > discard(List< T > input, Predicate<T> f) {
 		List<T> result = new LinkedList<T>();
-		
+		for ( T item : input ) {
+			if ( !f.test( item )) {
+				result.add( item );
+			}
+		}
 		return result;
 	}
 }
